927d2a8f0fa5ffa2d87970d0d4c8498471947175
james <0nlyoung7@gmail.com>
2014-09-20 23:19:20 +0900

    :gem: Add upload example

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use( express.static(__dirname+'/') );
+app.use( express.static( path.join( __dirname, 'example') ) ) ;
 
 app.listen(9999);
